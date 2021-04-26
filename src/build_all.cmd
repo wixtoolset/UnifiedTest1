768f7aa0e3326d2b8c11f2baf6d4420ce7b5b8f3
Rob Mensching <rob@firegiant.com>
2021-04-26 11:31:05 -0700

    Implement integrated build process

---
+++ src/build_all.cmd
@@ -0,0 +1,58 @@
+@setlocal
+@pushd %~dp0
+
+@set _C=Debug
+:parse_args
+@if /i "%1"=="release" set _C=Release& shift
+@if not "%1"=="" shift & goto parse_args
+
+@echo build %_C%
+
+md ..\build\artifacts
+
+:: DTF
+
+call dtf\dtf.cmd %_C% || exit /b
+
+
+:: internal
+
+call internal\internal.cmd %_C% || exit /b
+
+
+:: libs
+
+call libs\libs.cmd %_C% || exit /b
+
+
+:: api
+
+call api\api.cmd %_C% || exit /b
+
+
+:: burn
+
+call burn\burn.cmd %_C% || exit /b
+
+
+:: wix
+
+call wix\wix.cmd %_C% || exit /b
+
+
+:: ext
+
+call ext\ext.cmd %_C% || exit /b
+
+
+:: samples
+
+:: call samples\samples.cmd %_C% || exit /b
+
+
+:: integration tests
+
+call test\test.cmd %_C% || exit /b
+
+@popd
+@endlocal
